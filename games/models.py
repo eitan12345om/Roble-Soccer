889806a00daff0448ea84f91119f7657503fa843
Eitan Simler <eitan.simler@gmail.com>
2020-01-07 23:16:56 -0800

    Add new db column (#14)
    Add min_player column

--- games/models.py
+++ games/models.py
@@ -1,15 +1,31 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.timezone import localtime, now
 
 
 class Game(models.Model):
-    DEFAULT_NUM_PLAYERS = 18
+    DEFAULT_MAX_NUM_PLAYERS = 18
+    DEFAULT_MIN_NUM_PLAYERS = 14
 
     location = models.CharField(max_length=100, default='Roble Field')
     datetime = models.DateTimeField('when')
-    max_players = models.PositiveSmallIntegerField(default=DEFAULT_NUM_PLAYERS)
+    max_players = models.PositiveSmallIntegerField(default=DEFAULT_MAX_NUM_PLAYERS)
+    min_players = models.PositiveSmallIntegerField(default=DEFAULT_MIN_NUM_PLAYERS)
 
     def __str__(self):
         return f'Game at {self.location} on {self.datetime}'
+
+    def clean(self):
+        super().clean()
+
+        if self.datetime:
+            present = localtime(now())
+            if self.datetime < present:
+                raise ValidationError('A new game must be set in the future')
+
+        if self.max_players and self.min_players:
+            if self.max_players < self.min_players:
+                raise ValidationError('Max players is less than min players')
 
 
 class Player(models.Model):
--- games/tests/forms/test_game_form.py
+++ games/tests/forms/test_game_form.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+
 from django.test import TestCase
 
 from games.forms import GameForm
@@ -7,8 +9,9 @@ class GameFormTest(TestCase):
     def test_valid_data(self):
         form = GameForm({
             'location': 'Roble Field',
-            'datetime': '2019-12-03',
-            'max_players': 12
+            'datetime': datetime.now(tz=timezone.utc) + timedelta(days=1),
+            'max_players': 12,
+            'min_players': 10,
         })
 
         self.assertTrue(form.is_valid())
@@ -21,4 +24,5 @@ class GameFormTest(TestCase):
             'location': ['This field is required.'],
             'datetime': ['This field is required.'],
             'max_players': ['This field is required.'],
+            'min_players': ['This field is required.'],
         })
