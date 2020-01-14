1e59d4342c06928080a7920f16b001a328d395a6
Eitan Simler <eitan.simler@gmail.com>
2020-01-13 23:49:17 -0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                loader: 'css-loader'
+            }
+        ]
+    }
+};
+
